a3d75b05ebf11268953f1b19682b1b30aa3cad3d
souran7 <jashsouran1998@gmail.com>
2024-05-19 22:21:22 +0530

    host

--- infinite_edulocus/edulocus/settings.py
+++ infinite_edulocus/edulocus/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY ='0h07n(^mkhld4tthgj%9fj626h$wm36mapajw_t6-!cu+)48l9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'infiniteedulocus.com','wwww.infiniteedulocus.com', '35.154.61.89']
 LOGIN_URL = 'login'
 
 # Application definition
